7fe8ab444403e7cfec9de43c1cfa61176b819e46
mineWhale <747355875@qq.com>
2023-08-09 01:21:56 +0800

    first

---
+++ src/main/java/com/minewhale/grabber/grabbercore/GrabberCoreApplication.java
@@ -0,0 +1,13 @@
+package com.minewhale.grabber.grabbercore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrabberCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrabberCoreApplication.class, args);
+    }
+
+}
